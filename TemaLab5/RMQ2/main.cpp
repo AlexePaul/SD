864a0e20f564d3d47c5d77ebed382a03e70111d3
AlexePaul <alexepaul2011@yahoo.com>
2022-05-11 00:04:12 +0300

    Finished RMQ

--- TemaLab5/RMQ2/main.cpp
+++ TemaLab5/RMQ2/main.cpp
@@ -6,20 +6,33 @@ using namespace std;
 ifstream fin("rmq.in");
 ofstream fout("rmq.out");
 
-int rmq[100005][100005]={INT_MAX},N,M,x,y;
+const int maxN = 100005;
+const int LOG = 20;
+int rmq[maxN][LOG],N,M,x,y;
+
+int findMin(int x, int y){
+	int h = 0;
+	while(1 << (h+1) <= y-x+1){
+		h++;
+	}
+	cout << h << '\n';
+	return min(rmq[x][h], rmq[y-(1<<h)+1][h]);
+}
 
 int main(){
 	fin >> N >> M;
 	for(int i = 0; i < N; ++i){
-		fin >> x;
-		for(int j = 0; j <= i+1; ++i){
-			rmq[i][j] = min(rmq[i-1][j],x);
+		fin >> rmq[i][0];
+	}
+	// construiesc sparse table-ul in O(nlog(n))
+	for(int j = 1; j < LOG; ++j){
+		for(int i = 0; i + (1<<j) - 1 < N; ++i){
+			rmq[i][j] = min(rmq[i][j-1],rmq[i+(1<<(j-1))][j-1]); 
 		}
 	}
-	for(int i = 0; i < N; ++i){
-		for(int j = 0; j < N; ++j){
-			cout << rmq[i][j] << ' ';
-		}
-		cout << '\n';
+	//raspund in O(m) la toate intrebarile
+	for(int i = 0; i < M; ++i){
+		fin >> x >> y;
+		fout << findMin(x-1,y-1) << '\n';
 	}
 }
